7e7e2598e410765811c066e2504181c85b88e552
Matheus Ramos Silva <122408825+TH3USS@users.noreply.github.com>
2024-05-07 11:03:08 -0300

    Update nivelHeroi.js

--- nivelHeroi.js
+++ nivelHeroi.js
@@ -1,22 +1,24 @@
 var nome = "Arnaldinho";
-var xp = 11000;
+var vitorias = 200;
+var derrotas = 20;
+var xp = vitorias - derrotas;
 let nivel;
 
-if(xp <= 1000){
+if(xp <= 10){
     nivel = "Ferro";
-} else if (xp>=1001 && xp<=2000){
+} else if (xp>=11 && xp<=20){
     nivel = "Bronze";
-} else if (xp>=2001 && xp<=5000){
+} else if (xp>=21 && xp<=50){
     nivel = "Prata";
-} else if (xp>=5001 && xp<=7000){
+} else if (xp>=51 && xp<=70){
     nivel = "Ouro";
-} else if (xp>=7001 && xp<=8000){
+} else if (xp>=71 && xp<=80){
     nivel = "Platina";
-} else if (xp>=8001 && xp<=9000){
+} else if (xp>=81 && xp<=90){
     nivel = "Ascendente";
-} else if (xp>=9001 && xp<=10000){
+} else if (xp>=91 && xp<=100){
     nivel = "Imortal";
-}  else if (xp>=10001){
+}  else if (xp>=101){
     nivel = "Radiante";
 }
 
